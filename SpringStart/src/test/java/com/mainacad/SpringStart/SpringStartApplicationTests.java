496b4303a46ff938523345249a13ef40d455c22a
Antoninadan <mail100@i.ua>
2019-12-23 12:58:30 +0200

    initial commit

---
+++ SpringStart/src/test/java/com/mainacad/SpringStart/SpringStartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mainacad.SpringStart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppRunnerTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
